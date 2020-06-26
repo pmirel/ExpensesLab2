b0ff177e05943421e970900c6d666fa7f1e9021a
M <M@DESKTOP-J0D963F>
2020-06-26 12:04:43 +0300

    Added Angular Material

--- ExpensesLab2/ClientApp/src/app/app.module.ts
+++ ExpensesLab2/ClientApp/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { AngularMaterialModule } from './shared/angular-material.module';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ExpenseDetailComponent } from './expense-detail/expense-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -35,8 +38,11 @@ import { ExpenseDetailComponent } from './expense-detail/expense-detail.componen
                 path: 'fetch-data/:expenseId',
                 component: ExpenseDetailComponent
             }
-    ])
-  ],
+        ]),
+        AngularMaterialModule,
+        BrowserAnimationsModule
+    ],
+    exports: [AngularMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
